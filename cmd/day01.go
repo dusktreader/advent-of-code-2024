4ab598432280713c7e0ff9d19c796ffd5328d008
Tucker Beck <tucker.beck@gmail.com>
2024-12-02 22:16:59 -0800

    Completed Day 1

--- cmd/day01.go
+++ cmd/day01.go
@@ -14,20 +14,42 @@ import (
 )
 
 func init() {
-	rootCmd.AddCommand(day1Cmd)
-	day1Cmd.Flags().StringP("input-file", "i", "", "Get input from a file instead of stdin")
+	rootCmd.AddCommand(d1Cmd)
+	d1Cmd.PersistentFlags().StringP("input-file", "i", "", "Get input from a file instead of stdin")
+	d1Cmd.AddCommand(d1p1Cmd)
+	d1Cmd.AddCommand(d1p2Cmd)
 }
 
-var day1Cmd = &cobra.Command{
-	Use: "day01-1",
-	Short: "Day 1 Solution",
+var d1Cmd = &cobra.Command{
+	Use:   "day01",
+	Short: "Day 1 Solutions",
+	Long:  "The solutions for day 1 of Advent of Code 2024",
+	Run:   d1Main,
+}
+
+var d1p1Cmd = &cobra.Command{
+	Use: "part1",
+	Short: "Day 1, 1 Solution",
 	Long:  "The solution for day 1, part 1 of Advent of Code 2024",
-	Run:   day1Main,
+	Run:   d1p1Main,
 }
 
-func day1Main(cmd *cobra.Command, args []string){
+var d1p2Cmd = &cobra.Command{
+	Use: "part2",
+	Short: "Day 1, 2 Solution",
+	Long:  "The solution for day 1, part 2 of Advent of Code 2024",
+	Run:   d1p2Main,
+}
+
+func d1Main(cmd *cobra.Command, args []string){
+	_ = cmd.Help()
+}
+
+func getInput(cmd *cobra.Command, args []string) (left []int, right []int, err error) {
 	inputFile, err := cmd.Flags().GetString("input-file")
-	MaybeDie(err)
+	if err != nil {
+		return left, right, fmt.Errorf("Couldn't get input-file argument: %#v", err)
+	}
 
 	var input []byte
 
@@ -38,53 +60,96 @@ func day1Main(cmd *cobra.Command, args []string){
 		slog.Debug("No input file provided. Reading from stdin")
 		input, err = io.ReadAll(os.Stdin)
 	}
-	MaybeDie(err)
+	if err != nil {
+		return left, right, fmt.Errorf("Couldn't read input: %#v", err)
+	}
 
 	inputStr := string(input)
 	if inputStr == "" {
-		Die("Didn't get any input")
+		return left, right, fmt.Errorf("Didn't get any input")
 	}
 
 	inputStr = strings.TrimSpace(inputStr)
 	inputStr = strings.ReplaceAll(inputStr, "\n", " ")
 
-	var leftNumbers []int
-	var rightNumbers []int
 	isLeft := true
 
 	tokens := strings.Split(inputStr, " ")
 	for _, token := range tokens {
 		if token != "" {
 			number, err := strconv.Atoi(token)
-			MaybeDie(err)
+			if err != nil {
+				return left, right, fmt.Errorf("Failed to convert a token: %#v", err)
+			}
 			if isLeft {
-				leftNumbers = append(leftNumbers, number)
+				left = append(left, number)
 			} else {
-				rightNumbers = append(rightNumbers, number)
+				right = append(right, number)
 			}
 			isLeft = !isLeft
 		}
 	}
+	return left, right, nil
+}
 
-	distance, err := TotalDistance(leftNumbers, rightNumbers)
+func d1p1Main(cmd *cobra.Command, args []string){
+	left, right, err := getInput(cmd, args)
 	MaybeDie(err)
 
-	slog.Debug("Results:", "distance", distance, "left", leftNumbers, "right", rightNumbers)
+	distance, err := TotalDistance(left, right)
+	MaybeDie(err)
+
+	slog.Debug("Results:", "distance", distance, "left", left, "right", right)
 	fmt.Printf("%v\n", distance)
 }
 
-func TotalDistance(leftNumbers []int, rightNumbers []int) (int, error) {
-	if len(leftNumbers) != len(rightNumbers) {
+func d1p2Main(cmd *cobra.Command, args []string){
+	left, right, err := getInput(cmd, args)
+	MaybeDie(err)
+
+	similarity, err := Similarity(left, right)
+	MaybeDie(err)
+
+	slog.Debug("Results:", "similarity", similarity, "left", left, "right", right)
+	fmt.Printf("%v\n", similarity)
+}
+
+func TotalDistance(left []int, right []int) (int, error) {
+	if len(left) != len(right) {
 		return 0, fmt.Errorf("Need an even number of number inputs")
 	}
 
-	sort.Ints(leftNumbers)
-	sort.Ints(rightNumbers)
+	sort.Ints(left)
+	sort.Ints(right)
 
 	var total int
 
-	for i := 0; i < len(leftNumbers); i++ {
-		total += util.AbsInt(leftNumbers[i] - rightNumbers[i])
+	for i := 0; i < len(left); i++ {
+		total += util.AbsInt(left[i] - right[i])
 	}
+	return total, nil
+}
+
+func Similarity(left []int, right []int) (int, error) {
+	if len(left) != len(right) {
+		return 0, fmt.Errorf("Need an even number of number inputs")
+	}
+
+	leftMap := make(map[int]int)
+	rightMap := make(map[int]int)
+	allKeys := make(map[int]bool)
+
+	for i := 0; i < len(left); i++ {
+		leftMap[left[i]]++
+		allKeys[left[i]] = true
+		rightMap[right[i]]++
+		allKeys[right[i]] = true
+	}
+
+	var total int
+	for key, _ := range allKeys {
+		total += key * leftMap[key] * rightMap[key]
+	}
+
 	return total, nil
 }
--- cmd/day01_test.go
+++ cmd/day01_test.go
@@ -8,11 +8,11 @@ import (
 )
 
 func TestTotalDistanceSuccess(t *testing.T) {
-	leftNumbers := util.Shuffle([]int{ 1, 2, 3, 4 })
-	rightNumbers := util.Shuffle([]int{ 5, 6, 7, 8 })
+	left  := util.Shuffle([]int{ 1, 2, 3, 4 })
+	right := util.Shuffle([]int{ 5, 6, 7, 8 })
 
 	want := 4 * 4
-	got, err := cmd.TotalDistance(leftNumbers, rightNumbers)
+	got, err := cmd.TotalDistance(left, right)
 
 	if err != nil {
 		t.Fatalf(`TotalDistance failed with error: %#q`, err)
@@ -22,10 +22,10 @@ func TestTotalDistanceSuccess(t *testing.T) {
 		t.Fatalf(`TotalDistance failed on values: want %v, got %v`, want, got)
 	}
 
-	leftNumbers = util.Shuffle([]int{ -1, -2, -3, -4 })
-	rightNumbers = util.Shuffle([]int{ -5, -6, -7, -8 })
+	left = util.Shuffle([]int{ -1, -2, -3, -4 })
+	right = util.Shuffle([]int{ -5, -6, -7, -8 })
 
-	got, err = cmd.TotalDistance(leftNumbers, rightNumbers)
+	got, err = cmd.TotalDistance(left, right)
 
 	if err != nil {
 		t.Fatalf(`TotalDistance failed with error: %#q`, err)
@@ -37,10 +37,39 @@ func TestTotalDistanceSuccess(t *testing.T) {
 }
 
 func TestTotalDistanceFailLengths(t *testing.T) {
-	leftNumbers := util.Shuffle([]int{ 1, 2, 3, 4 })
-	rightNumbers := util.Shuffle([]int{ })
+	left := util.Shuffle([]int{ 1, 2, 3, 4 })
+	right := util.Shuffle([]int{ })
 
-	_, err := cmd.TotalDistance(leftNumbers, rightNumbers)
+	_, err := cmd.TotalDistance(left, right)
+
+	if err == nil {
+		t.Fatalf(`Did not get error!`)
+
+	}
+}
+
+func TestSimilaritySuccess(t *testing.T) {
+	left  := util.Shuffle([]int{ 1, 2, 3, 4, 5, 4, 3, 2, 1 })
+	right := util.Shuffle([]int{ 8, 7, 6, 5, 4, 5, 6, 7, 8 })
+
+	want := (1 * 2 * 0) + (2 * 2 * 0) + (3 * 2 * 0) + (4 * 2 * 1) + (5 * 1 * 2) + (6 * 0 * 2) + (7 * 0 * 2) + (8 * 0 * 2)
+	got, err := cmd.Similarity(left, right)
+
+	if err != nil {
+		t.Fatalf(`Similarity failed with error: %#q`, err)
+
+	}
+	if got != want {
+		t.Fatalf(`Similarity failed on values: want %v, got %v`, want, got)
+	}
+
+}
+
+func TestSimilarityFailLengths(t *testing.T) {
+	left := util.Shuffle([]int{ 1, 2, 3, 4 })
+	right := util.Shuffle([]int{ })
+
+	_, err := cmd.Similarity(left, right)
 
 	if err == nil {
 		t.Fatalf(`Did not get error!`)
